10485865f919270685ad0ca3096e9749c0a87d25
devinarena <d3v1n302418@gmail.com>
2022-05-19 16:03:53 -0400

    fixed bugs

--- oof.py
+++ oof.py
@@ -7,6 +7,7 @@
 ########################################################################################
 
 import sys
+import os
 
 import lexer
 import parser
@@ -25,6 +26,9 @@ ERROR = False
 RUNTIME_ERROR = False
 
 def runFile(file: str) -> None:
+    if not os.path.exists(file):
+        print("Error:: File not found")
+        exit(1)
     with open(file, "r") as source:
         run(source.read())
     if ERROR:
@@ -69,20 +73,14 @@ def runtime_error(token: token.Token, msg: str) -> None:
     exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
         print(f"Oof v{VERSION_MAJOR}.{VERSION_MINOR} created by Devin Arena")
         print("Run oof --help for more information")
         exit(0)
 
     if sys.argv[1] == "--help":
         print(f"Oof v{VERSION_MAJOR}.{VERSION_MINOR} created by Devin Arena")
-        print("Usage: oof.py <[i]nterpret> <file>")
+        print("Usage: oof.py <file>")
         exit(0)
-    elif sys.argv[1] == "c" or sys.argv[1] == "compile":
-        print("ERROR: Compiler not written yet")
-    elif sys.argv[1] == "i" or sys.argv[1] == "interpret":
-        runFile(sys.argv[2])
     else:
-        print(f"Oof v{VERSION_MAJOR}.{VERSION_MINOR} created by Devin Arena")
-        print("Run oof --help for more information")
-        exit(0)
+        runFile(sys.argv[1])
